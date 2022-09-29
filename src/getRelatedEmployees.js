beef148ffa332782cf52e3a1fa38f5bb084bbffe
Yago Castro <yago.fanstour@gmail.com>
2022-09-29 20:02:40 -0300

    Entrega requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const employeManager = data.employees.some(({ managers }) => managers.includes(id));
+
+  return employeManager;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const colab = data.employees.filter(({ managers }) => managers.includes(managerId))
+    .map((worker) => `${worker.firstName} ${worker.lastName}`);
+
+  return colab;
 }
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
