7a87b81fd8fbf0cf792811a94cb1a3f6f73722f9
Yago Castro <yago.fanstour@gmail.com>
2022-09-30 14:41:11 -0300

    Entrega requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(...animal) {
+  const salva = {};
+  if (animal.length === 0) {
+    data.species.forEach((specie) => {
+      salva[specie.name] = specie.residents.length;
+    });
+    return salva;
+  }
+  const nomeAnimal = animal.map((nome) => nome.specie);
+  const sexoAnimal = animal.map((nome) => nome.sex).toString();
+  const achaAnimal = species.find((specie) => nomeAnimal.toString() === specie.name);
+  if (sexoAnimal === 'male' || sexoAnimal === 'female') {
+    const qntdAnimalSex = species.find((specie) => nomeAnimal.toString() === specie.name)
+      .residents.filter((sexo) => sexo.sex === sexoAnimal).length;
+    return qntdAnimalSex;
+  }
+
+  return achaAnimal.residents.length;
 }
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 
 module.exports = countAnimals;
