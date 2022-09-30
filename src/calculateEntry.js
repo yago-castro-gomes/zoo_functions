52e12e27bc32ef1c47128df8f7ecbf12c565563e
Yago Castro <yago.fanstour@gmail.com>
2022-09-30 16:30:48 -0300

    Entrega requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const kids = entrants.filter((criança) => criança.age < 18).length;
+  const adults = entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length;
+  const olds = entrants.filter((old) => old.age >= 50).length;
+  return {
+    child: kids,
+    adult: adults,
+    senior: olds,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const func = countEntrants(entrants);
+  const kidsPrice = func.child * 20.99;
+  const adultPrice = func.adult * 49.99;
+  const seniorPrice = func.senior * 24.99;
+  return kidsPrice + adultPrice + seniorPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };
