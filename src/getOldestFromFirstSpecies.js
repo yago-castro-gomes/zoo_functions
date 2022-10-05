06192e3a04d4cbdd87884cbe9f039fe8ab328025
Yago Castro <yago.fanstour@gmail.com>
2022-10-04 21:51:39 -0300

    entrega 80% dos requisitos

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const specieEmploy = employees.find((employ) => employ.id === id).responsibleFor[0];
+  const specieAnimal = species.find((specie) => specie.id === specieEmploy).residents;
+  const old = specieAnimal.reduce((acc, curr) => {
+    if (acc.age > curr.age) {
+      return acc;
+    }
+    return curr;
+  }, []);
+  const arrayOld = Object.values(old);
+
+  return arrayOld;
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,36 @@
+const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const dayWeek = Object.keys(hours);
+
+const func = () => {
+  const craeteObj = dayWeek.reduce((acc, curr) => {
+    acc[curr] = {
+      officeHour: `Open from ${hours[curr].open}am until ${hours[curr].close}pm`,
+      exhibition: species.filter((specie) => specie.availability.includes(curr))
+        .map((spi) => spi.name),
+    };
+    return acc;
+  }, {});
+  craeteObj.Monday = {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  };
+  return craeteObj;
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (!scheduleTarget) {
+    return func();
+  }
+  const who = species.some((specie) => specie.name === scheduleTarget);
+  if (!who && !dayWeek.includes(scheduleTarget)) {
+    return func();
+  }
+  if (dayWeek.includes(scheduleTarget)) {
+    return { [scheduleTarget]: func()[scheduleTarget] };
+  }
+  return species.find((specie) => specie.name === scheduleTarget).availability;
 }
 
 module.exports = getSchedule;
